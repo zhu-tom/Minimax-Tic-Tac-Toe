63af2089aee799edbb4e3465f0e90520944e8478
zhu-tom <zhutom01@gmail.com>
2020-02-25 13:05:03 -0500

    commit

--- app/src/test/java/com/example/assignment2/TicTacToeGameUnitTest.java
+++ app/src/test/java/com/example/assignment2/TicTacToeGameUnitTest.java
@@ -5,5 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TicTacToeGameUnitTest {
-    
+    @Test
+    public void playGame() {
+        TicTacToeGame game = new TicTacToeGame();
+        game.userPlay(5);
+        game.cpuPlay();
+        game.userPlay(3);
+        game.cpuPlay();
+        game.userPlay(4);
+        game.cpuPlay();
+        game.userPlay(2);
+        game.cpuPlay();
+        game.userPlay(9);
+        assertEquals(new int[]{0,0,0}, game.checkWinner());
+    }
 }
